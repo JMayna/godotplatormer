a0136c68db9a3e39a98149646c7ba2ce2f22a360
Jordan Maynard <j.maynard.20122@gmail.com>
2024-06-03 21:12:15 -0400

    v.1.4 (Added Enemy, Double Jump, Crouch, Animations)

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,17 +9,27 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+var standing_cshape = preload("res://Resources/Player_standing_cshape.tres")
+var crouching_cshape = preload("res://Resources/player_crouching_cshape.tres")
+@onready var cshape= $CollisionShape2D
 
+#jump count
+var jump_count = 0
+var max_jumps = 2
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if is_on_floor():
+		jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -37,9 +47,14 @@ func _physics_process(delta):
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
+			cshape.shape = standing_cshape
 		else:
 			animated_sprite.play("walk")
-	
+			cshape.shape = standing_cshape
+	if is_on_floor():
+		if Input.is_action_pressed("crouch"):
+			animated_sprite.play("crouch")
+			cshape.shape = crouching_cshape
 	#apply movement 
 	if direction:
 		velocity.x = direction * SPEED
